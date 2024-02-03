cb451dce5d2e74c3d60ed712b7450b1d26fb8b61
Davide Buoso <daviibuoso@gmail.com>
2024-02-03 10:17:23 +0100

    requirements and training fixed

--- train.py
+++ train.py
@@ -9,40 +9,15 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./logs/',
                                          name_prefix='rl_model')
 
-MODE="train"
 env = gym.make("PandaReach-v3")
 
-if MODE == "test":
-    observation, _ = env.reset()
-    for _ in range(1000):
-        current_position = observation["observation"][0:3]
-        desired_position = observation["desired_goal"][0:3]
-        action = 5.0 * (desired_position - current_position)
-        observation, reward, terminated, truncated, info = env.step(action)
-        if terminated or truncated:
-            observation, info = env.reset()
-    env.close()
-else:
-    model = DDPG(
-        "MultiInputPolicy",
-        learning_rate=0.00001,
-        env=env,        
-        verbose=1,
-    )
+model = DDPG(
+    "MultiInputPolicy",
+    learning_rate=0.00001,
+    env=env,        
+    verbose=1,
+)
 
-    model.learn(30,callback=checkpoint_callback,)
-    env.close()
-    # EVALUATING
-    print("EVALUATING")
-    eval_env = gym.make("PandaReach-v3", render_mode="human")
-
-    # Random Agent, before training
-    mean_reward, std_reward = evaluate_policy(
-        model,
-        eval_env,
-        n_eval_episodes=10,
-        deterministic=True,
-    )
-
-    print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
-    eval_env.close()
+model.learn(100_000, callback=checkpoint_callback)
+env.close()
+model.save("DDPG_Model_final")
